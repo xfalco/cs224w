9a0abe90d4a343bc0c3f8eca5a89bb5d2029a01f
joefan123 <joefan123@gmail.com>
2013-10-29 17:55:04 -0400

    More exploration on votes

--- code/explore.sql
+++ code/explore.sql
@@ -50,3 +50,47 @@ WHERE   a.PostTypeId = b.Id
 AND     a.Id = c.PostId
 AND     (a.Id = 613183 OR a.ParentId = 613183)
 ORDER BY b.Name DESC, a.Score DESC, c.Score DESC;
+
+
+-- Votes on Question
+SELECT  b.Name,
+        COUNT(*)
+FROM    Votes a,
+        VoteTypes b
+WHERE   a.VoteTypeId = b.Id
+AND     a.PostId = 613183
+GROUP BY b.Name;
+
+
+-- Votes on Answers
+WITH tblVoteAnswer (PostId, PostScore, VoteType, NumVotes) AS
+(
+SELECT  c.Id,
+        c.Score,
+        b.Name,
+        COUNT(*)
+FROM    Votes a,
+        VoteTypes b,
+        Posts c,
+        PostTypes d
+WHERE   a.VoteTypeId = b.Id
+AND     a.PostId = c.Id
+AND     c.PostTypeId = d.Id
+AND     c.ParentId = 613183
+AND     d.Name = 'Answer'
+GROUP BY b.Name, c.Id, c.Score
+)
+SELECT  a.PostId,
+        a.NumVotes - b.NumVotes AS Score
+FROM    (
+        SELECT  PostId, NumVotes
+        FROM    tblVoteAnswer
+        WHERE   VoteType = 'UpMod'
+        ) a,
+        (
+        SELECT  PostId, NumVotes
+        FROM    tblVoteAnswer
+        WHERE   VoteType = 'DownMod'
+        ) b
+WHERE   a.PostId = b.PostId
+ORDER BY 2 DESC;
