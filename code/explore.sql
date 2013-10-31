c8266e66f598b7a4565de3affd38b36cc8909afb
joefan123 <joefan123@gmail.com>
2013-10-31 14:44:22 -0400

    Data mining (work in progress)

--- code/explore.sql
+++ code/explore.sql
@@ -102,7 +102,7 @@ SELECT  *
 FROM    Votes
 WHERE   PostId = 613218;   -- answer
 
-/*
+
 WITH tblVoteAnswer (PostId, PostScore, OwnerUserId, VoteType, NumVotes) AS
 (
 SELECT  c.Id,
@@ -128,7 +128,6 @@ SELECT  a.PostId,
              THEN CAST(a.NumVotes AS DOUBLE PRECISION) / (a.NumVotes + COALESCE(b.NumVotes,0))
              ELSE 0
         END AS PosEval
-
 FROM    (
         SELECT  PostId, OwnerUserId, NumVotes
         FROM    tblVoteAnswer
@@ -141,4 +140,113 @@ FROM    (
         ) b
         ON   a.PostId = b.PostId
 ORDER BY Score DESC;
-*/
+
+
+-- UserIDs and names of people in Question 613183 
+SELECT  CASE WHEN b.Name = 'Question'
+             THEN a.Id
+             ELSE a.ParentId
+        END AS QuestionId,
+        b.Name AS PostType,
+        a.Score,
+        a.Tags,
+        a.OwnerUserId,
+        c.DisplayName
+FROM    Posts a,
+        PostTypes b,
+        Users c
+WHERE   a.PostTypeId = b.Id
+AND     a.OwnerUserId = c.Id
+AND     (a.Id = 613183 OR a.ParentId = 613183)
+ORDER BY b.Name DESC, a.Score DESC;
+
+
+-- Network of people in Question 613183
+-- Edge attributes = (TotalVote)
+WITH tblQuestionUsers (QuestionId, PostType, Score, Tags, OwnerUserId, DisplayName) AS
+(
+SELECT  CASE WHEN b.Name = 'Question'
+             THEN a.Id
+             ELSE a.ParentId
+        END AS QuestionId,
+        b.Name AS PostType,
+        a.Score,
+        a.Tags,
+        a.OwnerUserId,
+        c.DisplayName
+FROM    Posts a,
+        PostTypes b,
+        Users c
+WHERE   a.PostTypeId = b.Id
+AND     a.OwnerUserId = c.Id
+AND     (a.Id = 613183 OR a.ParentId = 613183)
+)
+SELECT  a.QuestionId,
+        a.OwnerUserId AS SrcNodeId,
+        b.OwnerUserId AS DstNodeId,
+        a.Score AS TotalVote
+FROM    (
+        SELECT  a.QuestionId,
+                a.OwnerUserId,
+                a.Score
+        FROM    tblQuestionUsers a
+        WHERE   a.PostType = 'Answer'
+        ) AS a,
+        (
+        SELECT  a.QuestionId,
+                a.OwnerUserId
+        FROM    tblQuestionUsers a
+        WHERE   a.PostType = 'Question'
+        ) AS b
+WHERE   a.QuestionId = b.QuestionId
+ORDER BY TotalVote DESC;
+
+
+-- Up/down votes for questions and answers in Question 613183 (not scalable)
+WITH tblVotes (PostType, QuestionId, AnswerId, VoteType, NumVotes) AS 
+(
+SELECT  CAST('Answer' AS NVARCHAR(50)),
+        c.ParentId AS QuestionId,
+        c.Id AS AnswerId,
+        b.Name AS VoteType,
+        COUNT(*) AS NumVotes
+FROM    Votes a,
+        VoteTypes b,
+        Posts c,
+        PostTypes d
+WHERE   a.VoteTypeId = b.Id
+AND     a.PostId = c.Id
+AND     c.PostTypeId = d.Id
+AND     c.ParentId = 613183
+AND     d.Name = 'Answer'
+GROUP BY c.ParentId, c.Id, b.Name
+UNION ALL
+SELECT  'Question',
+        a.PostId AS QuestionId,
+        a.PostId,
+        b.Name AS VoteType,
+        COUNT(*) AS NumVotes
+FROM    Votes a,
+        VoteTypes b
+WHERE   a.VoteTypeId = b.Id
+AND     a.PostId = 613183
+GROUP BY a.PostId, b.Name
+)
+SELECT  a.PostType,
+        a.QuestionId,
+        a.AnswerId,
+        a.NumVotes AS UpVote,
+        COALESCE(b.NumVotes,0) AS DownVote,
+        a.NumVotes - COALESCE(b.NumVotes,0) AS Score
+FROM    (
+        SELECT  *
+        FROM    tblVotes a
+        WHERE   a.VoteType = 'UpMod'
+        ) a LEFT JOIN
+        (
+        SELECT  *
+        FROM    tblVotes a
+        WHERE   a.VoteType = 'DownMod'
+        ) b ON a.AnswerId = b.AnswerId
+ORDER BY PostType DESC, Score DESC;
+
